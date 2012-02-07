d75f00e325f4372abae97916f455232602eb18c9
Ashish Tiwari <ashishtiwari19@gmail.com>
2012-02-07 16:35:41 +0530

    jgeq file added

---
+++ jreq.php
@@ -0,0 +1,35 @@
+<?php
+
+?>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+<title>Simple form sending and receiving a JSON object to/from PHP</title>
+<script type="text/javascript" src="jQuery/jquery-1.4.4.js"></script>
+<script type="text/javascript" src="plugins/json2.js"></script>
+ 
+<script type="text/javascript"><!--
+$(document).ready(function(){ 
+  var data = 
+  { 
+    "sales": [ 
+      { "firstname" : "John", "lastname" : "Brown" },
+      { "firstname" : "Marc", "lastname" : "Johnson" }
+    ] // end of sales array
+  }
+  var dataString = JSON.stringify(data);
+  $.post('simpleformSubmit.php', { data: dataString}, showResult, "text");
+});
+ 
+function showResult(res)
+{
+  $("#fullresponse").html("Full response: " +res);
+  var obj = JSON.parse(res);
+  $("#sales1Lastname").html("Lastname of sales[1]: " +obj.sales[1].lastname);
+}
+//--></script>
+</head>
+<body>
+  <div id="fullresponse"><span/></div><br><br>
+  <div id="sales1Lastname"><span/></div>
+</body>
+</html> 
